bb9d6f0cba91cb8713c2a5dccb1bb0b8d988753c
NicoLaya23 <nicolaya23@gmail.com>
2022-10-19 20:56:16 -0300

    9

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 // const port = 3000;
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT || 3000;
 
 const publicFolderPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
